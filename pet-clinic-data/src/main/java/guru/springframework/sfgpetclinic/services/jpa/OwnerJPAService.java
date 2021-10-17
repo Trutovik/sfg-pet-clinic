6eb22f955d9f46fcb66a6a72f68dc69a9ee530c2
Aliaksandr Sabalevich <a.sabalevich@itechart-group.com>
2021-10-17 17:41:35 +0300

    Added spring data jpa service for vets. Closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/OwnerJPAService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/OwnerJPAService.java
@@ -15,13 +15,13 @@ import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
-public class OwnerJPA implements OwnerService {
+public class OwnerJPAService implements OwnerService {
 
   private final OwnerRepository ownerRepository;
   private final PetRepository petRepository;
   private final PetTypeRepository petTypeRepository;
 
-  public OwnerJPA(OwnerRepository ownerRepository, PetRepository petRepository
+  public OwnerJPAService(OwnerRepository ownerRepository, PetRepository petRepository
                         , PetTypeRepository petTypeRepository) {
     this.ownerRepository = ownerRepository;
     this.petRepository = petRepository;
